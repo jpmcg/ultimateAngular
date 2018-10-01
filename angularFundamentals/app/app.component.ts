bf580374e7dbef61a758cb78264b3562a89e32ac
sendjohnstuff@hotmail.com <sendjohnstuff@hotmail.com>
2018-10-01 07:47:54 +0100

    template fundamentals

--- angularFundamentals/app/app.component.ts
+++ angularFundamentals/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
+    <div class="app">      
+      <button (click)='handleClick(username.value)'>Change Name</button>
+      <input type='text' #username>
+      <p>{{ name }}</p>
     </div>
-  `
+  `,
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  name: string = 'John';
 
+
+  handleClick(value: string) {
+   console.log(value);
+  }
 }
